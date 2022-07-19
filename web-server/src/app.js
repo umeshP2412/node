d934153a526541c79ba48098c14b47daa2e83d7a
Umesh Patil <umeshpatil5124@gmail.com>
2022-07-19 23:20:12 +0530

    web-server changes 19 jul

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,23 +1,73 @@
+const path = require('path')
 const express = require('express');
+const hbs = require('hbs')
+
+console.log(__dirname)
+console.log(path.join(__dirname, '../public/'))
 
 const app = express()
 
+//Define Paths for Express config
+const pubDirectoryPath = path.join(__dirname, '../public/');
+const viewsPath = path.join(__dirname,'../templates/views');
+const partialPath = path.join(__dirname,'../templates/partials');
 
-app.get('/', (req, res) => {
-    const list = `
-    <h1 style='position: center'>************   Main page  ************</h1>
-    <li>apple</li>
-    <li>mango</li>
-    <li>tomato</li>
-    <li>orange</li>
-    <li>berry</li>
-    `
-    res.send(list)
+//Setup handlebar engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialPath)
+
+//Setup static directory to serve
+app.use(express.static(pubDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index',{
+        title: 'Weather',
+        name: 'Umesh'
+    })
 })
 
-app.get('/home', (req, res) => {
+app.get('/about', (req, res) => {
+    res.render('about',{
+        title: 'About',
+        name: 'Umesh',
+        imgPath: 'img/1.jpg'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        name: 'Umesh',
+        title: 'Help',
+        mobileNum: +91-9558829646,
+        email: 'umeshpatil2412@gmail.com',
+        msg: 'Cutomer Satisfaction is our top most priority!'
+    })
+})
+
+
+app.get('/weather', (req, res) => {
     
-    res.send('Hello Express!')
+    res.send({
+        forecast: 35,
+        location: 'Ahmedabad'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Umesh',
+        errorMsg: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Umesh',
+        errorMsg: 'Page not found'
+    })
 })
 
 app.listen(3000, () => {
