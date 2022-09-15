ddf1f77970b1aeadb31e324cacaf7d9c88c7f578
umesh.patil <umesh.patil@thomsonreuters.com>
2022-09-15 17:53:53 +0530

    new change on 15-09

---
+++ task-manager/src/email/account.js
@@ -0,0 +1,35 @@
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+const sendWelcomeEmail = async function() {
+
+    let testAccount = await nodemailer.createTestAccount();
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Umesh Patil 👻" umeshpatil5124@gmail.com', // sender address
+    to: "umeshpatil2412@gmail.com", // list of receivers
+    subject: "Hello Sagar", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+sendWelcomeEmail().catch(console.error)
